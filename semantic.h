79b75c934074c89959f78f2b048e2f10f5515bee
Gil Avraham <117095291+gil12av@users.noreply.github.com>
2025-05-10 21:48:21 +0300

    Update semantic.h

--- semantic.h
+++ semantic.h
@@ -4,6 +4,7 @@
 #include "y.tab.h"  
 #include "symbol_table.h"
 
+
 /* forward declaration for struct node (מוגדר ב‑parser.y) */
 struct node;              
 typedef struct node node; /* נוח לקצר */
@@ -29,5 +30,6 @@ int semCheckCall(Symbol *f, node *args);
 
 // Check bool type :
 int semCheckCondition(Type t);
+
 
 #endif
