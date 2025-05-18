29910bbf96ac1ca6aa83ded4fd37125129126a87
Gil Avraham <117095291+gil12av@users.noreply.github.com>
2025-05-18 18:00:48 +0300

    Update semantic.h

--- semantic.h
+++ semantic.h
@@ -28,7 +28,8 @@ int semCheckAssign(Type lhs, Type rhs);
 int semCheckReturn(Type ret);
 int semCheckCall(Symbol *f, node *args);
 
-// Check bool type //
+// Check bool type :
 int semCheckCondition(Type t);
+
 
 #endif
